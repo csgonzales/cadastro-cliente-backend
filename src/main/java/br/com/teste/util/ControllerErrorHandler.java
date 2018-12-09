61a9e17a3ed5e7e5cfac5ca60795a2e957a26b65
Caio Salustiano Gonzales <csgonzales1745@gmail.com>
2018-12-09 19:38:47 -0200

    Validação em exceções de campos

--- src/main/java/br/com/teste/util/ControllerErrorHandler.java
+++ src/main/java/br/com/teste/util/ControllerErrorHandler.java
@@ -1,8 +1,16 @@
 package br.com.teste.util;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -13,8 +21,43 @@ public class ControllerErrorHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(value = { IllegalArgumentException.class, IllegalStateException.class })
 	protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
-		String bodyOfResponse = "This should be application specific";
-		return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.CONFLICT, request);
+		ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), Collections.emptyList());
+		
+		return handleExceptionInternal(ex, apiError, new HttpHeaders(), HttpStatus.CONFLICT, request);
+	}
+	
+	@Override
+	protected ResponseEntity<Object> handleBindException(BindException ex, HttpHeaders headers, HttpStatus status,
+			WebRequest request) {
+		List<String> errors = new ArrayList<>();
+	    for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+	        errors.add(error.getField() + ": " + error.getDefaultMessage());
+	    }
+	    for (ObjectError error : ex.getBindingResult().getGlobalErrors()) {
+	        errors.add(error.getObjectName() + ": " + error.getDefaultMessage());
+	    }
+	     
+	    ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), errors);
+	    
+	    return handleExceptionInternal(ex, apiError, headers, apiError.getStatus(), request);
+	}
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		List<String> errors = new ArrayList<>();
+	    for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+	        errors.add(error.getField() + ": " + error.getDefaultMessage());
+	    }
+	    for (ObjectError error : ex.getBindingResult().getGlobalErrors()) {
+	        errors.add(error.getObjectName() + ": " + error.getDefaultMessage());
+	    }
+	     
+	    ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), errors);
+	    
+	    return handleExceptionInternal(ex, apiError, headers, apiError.getStatus(), request);
 	}
 
+
+	
 }
