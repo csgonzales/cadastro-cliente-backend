3dc8728e42635063f503271e1ba2be4df5b3d747
Caio Salustiano Gonzales <csgonzales1745@gmail.com>
2018-12-10 04:10:28 -0200

    habilitando CORS

--- src/main/java/br/com/teste/controller/ClienteController.java
+++ src/main/java/br/com/teste/controller/ClienteController.java
@@ -3,6 +3,7 @@ package br.com.teste.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ import br.com.teste.service.dto.ClienteDTO;
 import br.com.teste.service.dto.ClienteRespostaDTO;
 
 @RestController
+@CrossOrigin
 @RequestMapping("clientes")
 public class ClienteController {
 
